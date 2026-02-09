5bd4454ba97721e65039e458f1dcb04739159c21
Nursulu10 <n.klyshbekova@hyundai.kz>
2026-02-09 19:16:14 +0500

    Add files via upload

---
+++ 5 урок.py
@@ -0,0 +1,27 @@
+#Первое задание
+a = int(input("Введите число: "))
+
+d = 0
+
+for i in range(10, a + 1):
+    d += i
+
+print("Сумма чисел:", d)
+
+#Второе занятие
+a = int(input("Введите число: "))
+
+for d in range(0, a+1):
+if d % 2 == 0:
+    print(d)
+    
+#Третье задание
+n = int(input("Введите число: "))
+
+summa = 0
+
+for i in range(0, n+1):
+    if i % 3 == 0:
+        summa += i
+
+print("Сумма чисел:", summa)
